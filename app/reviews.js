9ceecbba9a763983db154836217c70bff6876522
Emiliano <emp10999@gmail.com>
2023-05-09 17:31:49 -0300

    working on carousel reviews

--- app/reviews.js
+++ app/reviews.js
@@ -5,6 +5,7 @@ import { Carousel } from "antd";
 import { ReviewCard } from "./components/cards/reviewCard";
 import example from "./assets/images/resources/review-example.png";
 import example2 from "./assets/images/resources/review-example-2.png";
+import dynamic from "next/dynamic";
 
 export function Reviews() {
     return (
@@ -17,7 +18,10 @@ export function Reviews() {
                 </p>
             </div>
             <div className={styles.testimonies}>
-                <Carousel effect="scrollx" className={styles.reviewsCarousel}>
+                <CarouselNoSSR
+                    effect="scrollx"
+                    className={styles.reviews_carousel}
+                >
                     <ReviewCard
                         photo={example}
                         name="Maria la del Barrio"
@@ -34,29 +38,18 @@ export function Reviews() {
                         name="Daniela Polaca"
                         title="Estudiante de 2er año Montoya"
                         testimony="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Laborum et eum odit error earum quasi, officiis aliquam
-    nesciunt tempore numquam repellat cumque voluptates
-    asperiores similique reprehenderit rerum repellendus alias
-    fugiat ut culpa. Nam sed deleniti dolores, ducimus inventore
-    earum saepe?"
+                        Laborum et eum odit error earum quasi, officiis aliquam
+                        nesciunt tempore numquam repellat cumque voluptates
+                        asperiores similique reprehenderit rerum repellendus alias
+                        fugiat ut culpa. Nam sed deleniti dolores, ducimus inventore
+                        earum saepe?"
                     />
-                </Carousel>
+                </CarouselNoSSR>
             </div>
         </div>
     );
 }
 
-/* 
-<ReviewCard
-    photo={example2}
-    name="Daniela Polaca"
-    title="Estudiante de 2er año Montoya"
-    testimony="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Laborum et eum odit error earum quasi, officiis aliquam
-    nesciunt tempore numquam repellat cumque voluptates
-    asperiores similique reprehenderit rerum repellendus alias
-    fugiat ut culpa. Nam sed deleniti dolores, ducimus inventore
-    earum saepe?"
-/>
-
-*/
+const CarouselNoSSR = dynamic(async () => Carousel, {
+    ssr: false,
+});
